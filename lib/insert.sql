37ad6475bc062edfb59e6fffce87205a04cb607a
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-11-27 05:41:04 +0300

    initial commit ....done

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+
+  ('Rowdy', 15, 'M', 'black', 'aggressive', 1),
+
+  ('Tabitha', 7, 'F', 'dark brown', 'sweet', 1),
+
+  ('Sergeant Brown', 18, 'M', 'brown', 'disciplined', 1),
+
+  ('Melissa', 9, 'F', 'white', 'gentle', 1),
+
+  ('Grinch', 12, 'M', 'greenish brown', 'grumpy', 0),
+
+  ('Wendy', 4, 'F', 'blonde', 'curious', 1),
+
+  (NULL, 3, 'M', 'brown', 'unknown', 0);
